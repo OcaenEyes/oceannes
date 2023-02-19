f32432ade4116860bf1f3c79e7559e0fde62e460
OcaenEyes <1450136519@qq.com>
2023-02-20 00:05:31 +0800

    master

--- desktop/src/Cartridge.cpp
+++ desktop/src/Cartridge.cpp
@@ -2,8 +2,8 @@
  * @Author: OCEAN.GZY
  * @Date: 2023-02-15 00:40:49
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2023-02-15 00:41:16
- * @FilePath: /oceannes/desktop/src/Cartridge.cpp
+ * @LastEditTime: 2023-02-19 19:19:42
+ * @FilePath: \oceannes\desktop\src\Cartridge.cpp
  * @Description: 注释信息
  */
 #include "Cartridge.h"
--- desktop/src/Mapper.cpp
+++ desktop/src/Mapper.cpp
@@ -2,8 +2,8 @@
  * @Author: OCEAN.GZY
  * @Date: 2023-02-15 00:43:57
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2023-02-19 12:04:31
- * @FilePath: /oceannes/desktop/src/Mapper.cpp
+ * @LastEditTime: 2023-02-19 19:41:34
+ * @FilePath: \oceannes\desktop\src\Mapper.cpp
  * @Description: 注释信息
  */
 #include "Mapper.h"
@@ -28,4 +28,43 @@ NameTableMirroring Mapper::getNameTableMirroring()
 
 std::unique_ptr<Mapper> Mapper::createMapper(Mapper::Type mapper_t, Cartridge &cart, std::function<void()> intterupt_cb, std::function<void(void)> mirroring_cb)
 {
+    std::unique_ptr<Mapper> ret(nullptr);
+
+    switch (mapper_t)
+    {
+    case NORM:
+        /* code */
+        ret.reset(new MapperNROM(cart));
+        break;
+    case SxROM:
+        /* code */
+        ret.reset(new MapperSxROM(cart, mirroring_cb));
+        break;
+    case UxROM:
+        /* code */
+        ret.reset(new MapperUxROM(cart));
+        break;
+    case CNROM:
+        /* code */
+        ret.reset(new MapperCNROM(cart));
+        break;
+    case MMC3:
+        /* code */
+        ret.reset(new MapperMMC3(cart,intterupt_cb,mirroring_cb));
+        break;
+    case AxROM:
+        /* code */
+        break;
+    case GxROM:
+        /* code */
+        break;
+    case ColorDreams:
+        /* code */
+        break;
+
+    default:
+        break;
+    }
+
+    return ret;
 }
